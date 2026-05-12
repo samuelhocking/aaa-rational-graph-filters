dabe34ff75c9f7590728bdf545e27407e75577db
Sam Hocking <samuel.c.hocking@gmail.com>
2026-05-12 15:07:33 -0400

    Convert to sparse

--- src/adj_grid.m
+++ src/adj_grid.m
@@ -5,27 +5,61 @@ function A = adj_grid(nrow,ncol)
     end
     N = nrow*ncol;
     linear_idx = @(r,c) (r-1)*ncol+c;
-    A = zeros(N);
-    for i = 1:ncol
-        for j = 1:nrow
+    rr = [];
+    cc = [];
+    vv = [];
+    % A = zeros(N);
+    for i = 1:nrow
+        for j = 1:ncol
             disp([i,j])
             linidx = linear_idx(i,j);
             if j == 1
-                A(linidx, linear_idx(i,j+1)) = 1;
+                disp("j=1")
+                rr(end+1) = linidx;
+                cc(end+1) = linear_idx(i,j+1);
+                vv(end+1) = 1;
+                % A(linidx, linear_idx(i,j+1)) = 1;
             elseif j == ncol
-                A(linidx, linear_idx(i,j-1)) = 1;
+                disp("j=ncol")
+                rr(end+1) = linidx;
+                cc(end+1) = linear_idx(i,j-1);
+                vv(end+1) = 1;
+                % A(linidx, linear_idx(i,j-1)) = 1;
             else
-                A(linidx, linear_idx(i,j+1)) = 1;
-                A(linidx, linear_idx(i,j-1)) = 1;
+                disp("j=else")
+                rr(end+1) = linidx;
+                cc(end+1) = linear_idx(i,j+1);
+                vv(end+1) = 1;
+                rr(end+1) = linidx;
+                cc(end+1) = linear_idx(i,j-1);
+                vv(end+1) = 1;
+                % A(linidx, linear_idx(i,j+1)) = 1;
+                % A(linidx, linear_idx(i,j-1)) = 1;
             end
             if i == 1
-                A(linidx, linear_idx(i+1,j)) = 1;
+                disp("i=1")
+                rr(end+1) = linidx;
+                cc(end+1) = linear_idx(i+1,j);
+                vv(end+1) = 1;
+                % A(linidx, linear_idx(i+1,j)) = 1;
             elseif i == nrow
-                A(linidx, linear_idx(i-1,j)) = 1;
+                disp("i=nrow")
+                rr(end+1) = linidx;
+                cc(end+1) = linear_idx(i-1,j);
+                vv(end+1) = 1;
+                % A(linidx, linear_idx(i-1,j)) = 1;
             else
-                A(linidx, linear_idx(i+1,j)) = 1;
-                A(linidx, linear_idx(i-1,j)) = 1;
+                disp("i=else")
+                rr(end+1) = linidx;
+                cc(end+1) = linear_idx(i+1,j);
+                vv(end+1) = 1;
+                rr(end+1) = linidx;
+                cc(end+1) = linear_idx(i-1,j);
+                vv(end+1) = 1;
+                % A(linidx, linear_idx(i+1,j)) = 1;
+                % A(linidx, linear_idx(i-1,j)) = 1;
             end
         end
     end
+    A = sparse(rr,cc,vv,N,N);
 end
